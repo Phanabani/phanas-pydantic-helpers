c15ba78ba9c898f7b2af93d23c664834e420fd06
Phanabani <phanabani@gmail.com>
2023-02-05 01:12:54 -0500

    Make fields a package.

--- phanas_pydantic_helpers/fields/time.py
+++ phanas_pydantic_helpers/fields/time.py
@@ -2,15 +2,21 @@ from __future__ import annotations
 
 __all__ = ["DateTimeField", "TimeField", "DurationField"]
 
-import pendulum as pen
+from phanas_pydantic_helpers.common.imports import DummyModule, raise_if_missing_deps
+from phanas_pydantic_helpers.common.time import parse_duration, parse_time
+from phanas_pydantic_helpers.helpers import FieldConverter
 
-from server.common.pydantic.helpers import FieldConverter
-from server.common.time import parse_duration, parse_time
+try:
+    import pendulum as pen
+except ImportError:
+    pen = DummyModule("pendulum")
 
 
 class DateTimeField(pen.DateTime, FieldConverter):
     @classmethod
     def _pyd_convert(cls, datetime_str: str):
+        raise_if_missing_deps(pen)
+
         parsed_dt = pen.parse(datetime_str)
         if not isinstance(parsed_dt, pen.DateTime):
             raise ValueError("Invalid datetime format")
@@ -20,10 +26,12 @@ class DateTimeField(pen.DateTime, FieldConverter):
 class TimeField(pen.Time, FieldConverter):
     @classmethod
     def _pyd_convert(cls, time_str: str):
+        raise_if_missing_deps(pen)
         return parse_time(time_str, class_=cls)
 
 
 class DurationField(pen.Duration, FieldConverter):
     @classmethod
     def _pyd_convert(cls, duration_str: str):
+        raise_if_missing_deps(pen)
         return parse_duration(duration_str, class_=cls)
