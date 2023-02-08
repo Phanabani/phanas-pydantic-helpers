d2ef12b60bec8d7a3bc6bab554a51e7fc9499274
Phanabani <phanabani@gmail.com>
2023-02-08 12:41:13 -0500

    Add `FieldConverter` tests.

---
+++ tests/test_field_converter.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel
+
+from phanas_pydantic_helpers import FieldConverter
+
+
+def test_basic():
+    class StrToInt(int, FieldConverter):
+        @classmethod
+        def _pyd_convert(cls, value: str) -> int:
+            return int(value)
+
+    class Model(BaseModel):
+        x: StrToInt
+
+    m = Model(x="5")
+    assert m.x == 5
+
+
+def test_basic_with_suffix():
+    class StrToInt(int, FieldConverter):
+        @classmethod
+        def _pyd_convert_str(cls, value: str) -> int:
+            return int(value)
+
+    class Model(BaseModel):
+        x: StrToInt
+
+    m = Model(x="5")
+    assert m.x == 5
+
+
+def test_multiple_converters():
+    class StrToInt(int, FieldConverter):
+        @classmethod
+        def _pyd_convert_str(cls, value: str) -> int:
+            return int(value)
+
+        @classmethod
+        def _pyd_convert_bytes(cls, value: bytes) -> int:
+            return int.from_bytes(value, "big")
+
+    class Model(BaseModel):
+        x: StrToInt
+
+    m_str = Model(x="5")
+    assert m_str.x == 5
+
+    m_bytes = Model(x=b"\x00\xFF")
+    assert m_bytes.x == 0xFF
